a2287a8b3b61e577b4e645f53ffef8bf3e33b5ff
ryanp4096 <ryanp4096@gmail.com>
2023-10-15 18:49:52 -0700

    More advanced templates (fixed) see previous commit for details

--- admin/test.js
+++ admin/test.js
@@ -1,10 +1,12 @@
+// for testing fill
+
 const { Parser } = require('./fill.js');
 const { sampleDocument } = require('./sample.js');
 const fs = require('fs');
 
 function run() {
     const data = sampleDocument();
-    const template = fs.readFileSync('invoice.tex', 'utf-8');
+    const template = fs.readFileSync('admin/invoice.tex', 'utf-8');
     const result = (new Parser(template, data)).parse();
     console.log(result);
 }
